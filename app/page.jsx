2174efdf466812a9cd71b096e339e02b09ebe322
Avnish Kumar <69550090+theavnishkumar@users.noreply.github.com>
2024-05-09 02:53:09 +0530

    added animated cursor

--- app/page.jsx
+++ app/page.jsx
@@ -5,6 +5,7 @@ import Projects from "@/components/Projects";
 import "./globals.css";
 import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
+import AnimatedCursor from "react-animated-cursor"
 
 export default function Home() {
   return (
@@ -16,6 +17,27 @@ export default function Home() {
         <Projects />
         <Contact />
         <Footer />
+      <AnimatedCursor
+      innerSize={8}
+      outerSize={40}
+      color='0, 150, 0'
+      outerAlpha={0.2}
+      innerScale={0.9}
+      outerScale={1.5}
+      clickables={[
+        'a',
+        'input[type="text"]',
+        'input[type="email"]',
+        'input[type="number"]',
+        'input[type="submit"]',
+        'input[type="image"]',
+        'label[for]',
+        'select',
+        'textarea',
+        'button',
+        '.link'
+      ]}
+    />
       </main>
       <main className="min-[320px]:hidden flex flex-col items-center justify-center w-svw min-h-svh p-4">
         <p className="text-xl font-semibold">
